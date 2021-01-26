da38b2b7e5a2726d22acac5d0fbcdca68bc1f627
Juna <71682432+Junhwa1207-design@users.noreply.github.com>
2021-01-25 22:13:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express'); // like a PHP require
-const server = express();
 const path = require('path');
+
+const server = express();
+
 
 // set our views directory
 server.set("views", path.join(__dirname, 'views'));
 // set the static assets director so Express knows where to look
 //for css files, JS files, images et - anything static
-server.use(express.static(path(__dirname, 'public')));
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.get("/", (req, res) => {
   console.log('you have now hit the home route');
-  res.send('your first express app');
+  res.sendFile('views/index.html');
 })
 
 server.get("/contact", (req, res) => {
   console.log('hit the contact route');
-  res.send('contact page');
+  res.sendFile('views/contact.html');
 })
 
 const port = process.env.PORT || 3000; // localhost:3000
