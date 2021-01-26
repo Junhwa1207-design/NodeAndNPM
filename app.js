df4bfaa7390015e9a32953ad150c0508d7c22412
Juna <71682432+Junhwa1207-design@users.noreply.github.com>
2021-01-25 21:46:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
-const http = require('http'); // like a PHP require
-const stream = require('fs'); 
-//require is more or less the same as a JS import
+const express = require('express'); // like a PHP require
+const server = express(); 
 
-// const hostname = '127.0.0.1'; // this is localhost
+server.get("/", (req, res) => {
+  console.log('you have now hit the home route');
+  res.send('your first express app');
+})
+
+server.get("/contact", (req, res) => {
+  console.log('hit the contact route');
+  res.send('contact page');
+})
+
 const port = process.env.PORT || 3000; // localhost:3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-
-
-  let url = req.url; // i.e. localhost:3000/contact
-
-  switch (url) {
-    case "/":
-      stream.readFile('index.html' , null, ((err, data) => {
-        if (err) {
-          res.writeHead(404);
-          res.write('404 not found');
-        }else {
-          res.write(data);
-        }
-      })) 
-      break;
-    case "/contact":
-      stream.readFile('contact.html' , null, ((err, data) => {
-        if (err) {
-          res.writeHead(404);
-          res.write('404 not found');
-        }else {
-          res.write(data);
-        }
-      })) 
-      break;
-
-      case "/portfolio":
-        stream.readFile('portfolio.html' , null, ((err, data) => {
-          if (err) {
-            res.writeHead(404);
-            res.write('404 not found');
-          }else {
-            res.write(data);
-          }
-        })) 
-        break;
-
-        default:
-  res.end('Hello World'); // serve up a custom error page
-
-}
-});
-
 server.listen(port, () => {
-  console.log(`Server is now running at ${port}:/`);
+  console.log(`Server is now running at ${port}/`);
 });
+  
+  
+  
+  
